79cd5e11e076f2f3f03b8f2fd04473e3dbcb5a33
Javier Olmos Pérez <javier.olmos.perez@ces-vegamedia.es>
2025-05-19 00:31:38 +0200

    Ventana suscripciones

--- frontend/GastApp/src/api/urlConnection.ts
+++ frontend/GastApp/src/api/urlConnection.ts
@@ -1,2 +1,2 @@
 // Modificar cada que se reinicie el ngrok
-export const API_BASE_URL = 'https://35e1-5-180-230-103.ngrok-free.app';
+export const API_BASE_URL = 'https://d081-79-150-30-221.ngrok-free.app';
